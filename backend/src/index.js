1a35b070e292c103655e293faf7438c7e82e7eda
Nikhil Dhiman <dhimannikhil300@gmail.com>
2025-05-04 00:52:32 +0530

    create register controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send('Hello guys')
 })
+
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log("server is runing on 8080");
